60c5ef8778fd95c5c372fc0eba569942c683359c
JeffreyCao <jeffreycao1024@gmail.com>
2019-11-24 07:40:08 +0800

    ios icon info ok

--- flutter_logo_updater/ios_finder.py
+++ flutter_logo_updater/ios_finder.py
@@ -3,9 +3,39 @@
 
 from ezutils import readjson
 import os
+import re
+
+patternSize = re.compile(r'([0-9\.]+)x.*', re.I)
+patternScale = re.compile(r'([0-9\.]+)x', re.I)
+
+
+def re_app_icon_info(appicon):
+
+    scale = appicon['scale']
+    scale_match = patternScale.match(scale)
+    if scale_match:
+        new_scale = patternScale.match(scale).group(1)
+    else:
+        new_scale = 1
+
+    size = appicon['size']
+    size_match = patternSize.match(size)
+    if size_match:
+        new_size = int(float(size_match.group(1))*float(new_scale))
+    else:
+        new_size = 1024
+
+    filename = appicon['filename']
+    new_appicon = {"size": new_size,
+                   "filename": filename}
+    return new_appicon
 
 
 def get_app_icon_info(appiconset):
     path = os.path.join(appiconset, 'Contents.json')
-    json_obj = readjson(path)
-    return json_obj
+    appicons = readjson(path)['images']
+
+    new_appicons = []
+    for appicon in appicons:
+        new_appicons.append(re_app_icon_info(appicon))
+    return new_appicons
